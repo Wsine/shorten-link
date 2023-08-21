aa1577651cd9afeeab0340b50c0de7fd58c850e8
Wsine <weizhy3@hotmail.com>
2023-08-21 09:41:10 +0800

    add: ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import '@/styles/globals.css'
+import { Rubik } from 'next/font/google'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const rubik = Rubik({ subsets: ['latin'] })
+
+export const theme = extendTheme({
+  useSystemColorMode: true,
+  fonts: {
+    heading: 'var(--font-rubik)',
+    body: 'var(--font-rubik)',
+  }
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-rubik: ${rubik.style.fontFamily};
+          }
+        `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
